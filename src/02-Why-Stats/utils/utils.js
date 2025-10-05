befd63d3b80ebf8a9c85875ca201060286852fc5
lily-soetebier <lksoeteb@ncsu.edu>
2025-10-05 16:55:26 -0400

    added in the three level roll up function

--- src/02-Why-Stats/utils/utils.js
+++ src/02-Why-Stats/utils/utils.js
@@ -238,3 +238,43 @@ export const sumUpWithReducerTests = (reducerFunctions, reducerProperties, data,
  *     third level.
 **/
 
+export const threeLevelRollUpFlatMap = (data, level1Key, level2Key, level3Key, countKey) => {
+
+  // 1. Rollups on 3 nested levels
+  const colTotals = rollups(
+    data,
+    (v) => v.length, //Count length of leaf node
+    (d) => d[level1Key], //Accessor at 1st level
+      (d) => d[level2Key], //Accessor at 2nd level
+        (d) => d[level3Key],
+  )
+
+  // 2. Flatten 1st grouped level back to array of objects
+    const flatTotals = colTotals.flatMap( (l1Elem) => {
+
+      // 2.1 Assign level 1 key
+      let l1KeyValue = l1Elem[0]
+
+      // 2.2 Flatten 2nd grouped level
+      const twoFlatLevels = l1Elem[1].flatMap((l2Elem) => {
+
+        // 2.2.1 Assign level 2 key
+        let l2KeyValue = l2Elem[0]
+          const threeFlatLevels = l2Elem[1].flatMap((l3Elem) => {
+            let l3KeyValue = l2Elem[1],
+            
+            return {
+            [level1Key]: l1KeyValue,
+            [level2Key]: l2KeyValue,
+            [level3Key]: l3KeyValue,
+            [countKey]: l3Elem[1],
+            }
+        })
+
+      // 3. Return flattened array of objects
+      return twoFlatLevels
+    })
+    // 3. Return the sorted totals
+    return flatTotals
+  })
+}
