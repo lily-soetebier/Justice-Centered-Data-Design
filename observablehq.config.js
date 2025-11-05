28ddbfe652bb807c52e5a79b731bfeb6b37bd331
lily-soetebier <lksoeteb@ncsu.edu>
2025-11-04 19:54:47 -0500

    fixing title

--- observablehq.config.js
+++ observablehq.config.js
@@ -109,9 +109,13 @@ export default {
       open: false,
       pages: [
         {
-          name: "Weighting Words with TF-IDF",
+          name: "3.1 Weighting Words with TF-IDF",
           path: "/03-Textual-Analysis/01-weighting-words",
         },
+        {
+          name: "3.2 Processing Text",
+          path: "/03-Textual-Analysis/02-processing"
+          },
       ],
     },   
     // HELP PAGES
